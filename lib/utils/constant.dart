e2e2810eb6d325c79d36f025d8d311569a735ad9
Henry Tena <henry.tena@ffufm.com>
2023-10-26 18:40:54 +0800

    update constant

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -20,6 +20,14 @@ const codex = {
             'C++ was developed by Bjarne Stroustrup, as an extension to the C language. '
             'C++ gives programmers a high level of control over system resources and memory. '
             'The language was updated 4 major times in 2011, 2014, 2017, and 2020 to C++11, C++14, C++17, C++20.'
+      },
+      1: {
+        'topic': 'Introduction to C+++',
+        'heading': 'What is C++?',
+        'body': 'C++ is a cross-platform language that can be used to create high-performance applications. '
+            'C++ was developed by Bjarne Stroustrup, as an extension to the C language. '
+            'C++ gives programmers a high level of control over system resources and memory. '
+            'The language was updated 4 major times in 2011, 2014, 2017, and 2020 to C++11, C++14, C++17, C++20.'
       }
     },
     Categories.intermediate: {},
